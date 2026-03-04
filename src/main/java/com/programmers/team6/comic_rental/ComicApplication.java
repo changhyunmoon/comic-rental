e89b1199319733209f089cf5e33845bd3aeeff9f
changhyunmoon <67505983+changhyunmoon@users.noreply.github.com>
2026-03-04 21:11:35 +0900

    first commit

---
+++ src/main/java/com/programmers/team6/comic_rental/ComicApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.team6.comic_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programmers/team6/comic_rental/ComicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.team6.comic_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
